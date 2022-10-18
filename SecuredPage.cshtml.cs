d34d0b7dc8e9163c78d245b463977c4ba78518d6
ishfaq babar <babar_ravians@hotmail.com>
2022-10-18 23:16:59 +0500

    git ignore and add new razor page

---
+++ SecuredPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServices
+{
+    public class SecuredPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
